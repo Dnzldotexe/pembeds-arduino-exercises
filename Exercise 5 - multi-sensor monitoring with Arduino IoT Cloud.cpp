b3b72e4d4d7d7b6ad32b3c667da2738ab71d810b
Danziel Cempron <104302220+Dnzldotexe@users.noreply.github.com>
2025-02-11 21:09:15 +0800

    Create Exercise 5

---
+++ Exercise 5 - multi-sensor monitoring with Arduino IoT Cloud.cpp
@@ -0,0 +1,11 @@
+// INSTRUCTIONS
+// Exercise 5 builds on Exercise 4.
+// In Exercise 5, using your DHT11 or LM35 sensors, read and display on your cloud dashboard temperature and humidity readings, once every 5 seconds.
+// And then, add a water level sensor and display its reading on another display widget.  For this sensor, display the height of the water from the bottom edge of the sensor.
+// Programming requirement:  You must poll the sensors in round robin fashion, displaying the temperature and humidity readings every 5 seconds and water level reading every 8 seconds.  Hint:  Use a loop.
+// Submit a short video of your dashboard showing the temperature, humidity and water level display gauge widgets change values.  Submit also a copy of your code in a text file.  Write your name in the code as an inline comment.
+
+// CODE MADE BY: Danziel Cempron
+// https://github.com/Dnzldotexe/pembeds-arduino-exercises/
+
+// CODE STARTS HERE
